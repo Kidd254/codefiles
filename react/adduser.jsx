b8495ef23af220aca93d43f1a21b4551fb2db3e2
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-01-20 21:26:58 +0300

    Add method for submiting user data to api

--- react/adduser.jsx
+++ react/adduser.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
 const addUser = () => {
     const [formData, setFormData] = useState({
@@ -18,7 +19,7 @@ const addUser = () => {
     const handleSubmit = async(e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5000/api/users', {
+            const response = await axios.post('http://localhost:5000/api/users', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
